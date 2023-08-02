ec1a5d30c5956282ceb4614006ff51593469a1ab
Marty <70270238+martystanislavova@users.noreply.github.com>
2023-08-02 17:28:52 +0300

    Update Tester.java

--- sources/java/newDBSws22/src/Tester.java
+++ sources/java/newDBSws22/src/Tester.java
@@ -4,19 +4,6 @@ public class Tester {
 
 	public static void main(String[] args) {
 		DatabaseHelper dbHelper=new DatabaseHelper();
-		
-	
-		
-		/*for(int id:result) {
-			System.out.println(id);
-		}*/
-		
-		//TODO: your task: write a function which inserts to table professor  (Method InsertIntoProfessor in DatabaseHelper)
-
-		
-		//TODO: your task wirte a function which selects the professor id (Method selectProfessorIdFromProfessor in DatabaseHelper)
-		
-
 		dbHelper.close();
 	}
 
